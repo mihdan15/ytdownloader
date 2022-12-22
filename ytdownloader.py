dcb683a1de49662d500d67a6570ff294192d528e
Mihdan Advani <mihdan31@gmail.com>
2022-12-22 17:06:02 +0700

    tambah konfigurasi download

--- ytdownloader.py
+++ ytdownloader.py
@@ -12,20 +12,16 @@ def main():
 
     matches = ['audio', 'highest_resolution', 'lowest_resolution']
     if st.button("download"):
+        file_data = BytesIO()
         yt = YouTube(link)
         st.write("Judul :", yt.title)
         st.write("Total Tayangan :", yt.views)
         if option == 'audio':
-            yt.streams.get_audio_only().download() 
+            yt.streams.get_audio_only().download(output_stream=file_data) 
         elif option == 'highest_resolution':
-            yt.streams.get_highest_resolution().download()
+            yt.streams.get_highest_resolution().download(output_stream=file_data)
         elif option == 'lowest_resolution':
-            yt.streams.get_highest_resolution().download()
-
-
-        # Muat file ke dalam memori
-        file_data = BytesIO()
-        yt.download(output_stream=file_data)
+            yt.streams.get_highest_resolution().download(output_stream=file_data)
 
         # Unduh file dari memori ke disk
         st.download_file(file_data.getvalue(), f"{yt.title}.mp4")
